3614f5f55c7e2b5b6e85b8ce317118231ac0f2a2
David <rezke@hotmail.com>
2017-01-07 18:12:29 +0100

    Dodao crud funkcionalnost za Ingredient.

--- IvAnja/IvAnja.cs
+++ IvAnja/IvAnja.cs
@@ -1,26 +1,79 @@
 ﻿using System;
 using System.Windows.Forms;
 using DAL;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
 
 namespace IvAnja
 {
     public partial class IvAnja : Form
     {
+        IvAnjaEntities _context;
+        private List<Ingredient> ingredients = new List<Ingredient>();
+        BindingSource ingredientsBindingSource = new BindingSource();
+
+
         public IvAnja()
         {
             InitializeComponent();
+            _context = new IvAnjaEntities();
+            _context.Ingredients.Load();
+
+            ingredientsBindingSource.DataSource = _context.Ingredients.Local.ToBindingList();
+            lbIngredients.DataSource = ingredientsBindingSource;
+            lbIngredients.DisplayMember = "Name";
+
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
             Ingredient ingredient = new Ingredient();
 
-           using (IvAnjaEntities db = new IvAnjaEntities())
+
+            ingredient.Name = this.txtName.Text;
+            _context.Ingredients.Add(ingredient);
+            _context.SaveChanges();
+
+            _context.Ingredients.Load();
+            ingredientsBindingSource.ResetBindings(true);
+            lbIngredients.Refresh();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            Ingredient selectedIngredient = new Ingredient();
+
+            selectedIngredient = (Ingredient)lbIngredients.SelectedItem;
+            Ingredient ingredientToDelete = _context.Ingredients.Find(selectedIngredient.Id);
+            _context.Ingredients.Remove(ingredientToDelete);
+            _context.SaveChanges();
+
+            _context.Ingredients.Load();
+            ingredientsBindingSource.ResetBindings(true);
+            lbIngredients.Refresh();
+        }
+
+        private void brnEdit_Click(object sender, EventArgs e)
+        {
+            Ingredient selectedIngredient = new Ingredient();
+          
+
+            selectedIngredient = (Ingredient)lbIngredients.SelectedItem;
+            Ingredient ingredientToEdit = _context.Ingredients.Find(selectedIngredient.Id);
+            Ingredient newIngredient = new Ingredient();
+            newIngredient.Name = txtName.Text;
+            newIngredient.Id = selectedIngredient.Id;
+
+            if (ingredientToEdit != null)
             {
-                ingredient.Name = this.txtName.Text;
-                db.Ingredients.Add(ingredient);
-                db.SaveChanges();
-            }
+                _context.Entry(ingredientToEdit).CurrentValues.SetValues(newIngredient);
+                _context.SaveChanges();
+            }         
+
+            _context.Ingredients.Load();
+            ingredientsBindingSource.ResetBindings(true);
+            lbIngredients.Refresh();
         }
     }
 }
