7b7f5b731cae72b6360aaaf605c860584a77b46b
David <rezke@hotmail.com>
2017-01-07 02:28:20 +0100

    Added DAL to Solution          -added test Entity to database Model          -renamed Form1 to IvAnja and added test field for adding test data to database

---
+++ IvAnja/IvAnja.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+using DAL;
+
+namespace IvAnja
+{
+    public partial class IvAnja : Form
+    {
+        public IvAnja()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Ingredient ingredient = new Ingredient();
+
+           using (IvAnjaEntities db = new IvAnjaEntities())
+            {
+                ingredient.Name = this.txtName.Text;
+                db.Ingredients.Add(ingredient);
+                db.SaveChanges();
+            }
+        }
+    }
+}
